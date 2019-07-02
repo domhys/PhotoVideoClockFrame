b3950bddbadeb940b8bee71dd646ad3a675372c2
Dominik Hys <dominik.hys@gmail.com>
2019-07-02 10:26:42 +0200

    added example of a customizable animation from xml

--- app/src/main/java/com/example/photovideoclockframe/presentation/extensions/ImageViewExtension.kt
+++ app/src/main/java/com/example/photovideoclockframe/presentation/extensions/ImageViewExtension.kt
@@ -1,10 +1,12 @@
 package com.example.photovideoclockframe.presentation.extensions
 
 import android.widget.ImageView
+import com.bumptech.glide.GenericTransitionOptions
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.example.photovideoclockframe.R
 import com.example.photovideoclockframe.utility.RotateIfNeededTransformation
 import java.io.File
 
@@ -12,16 +14,19 @@ fun ImageView.loadAndRotateImage(path: String) {
     if (path.endsWith(".gif")) {
         Glide.with(this)
             .load(File(path))
-            .transition(DrawableTransitionOptions.withCrossFade(2000))
+            .transition(DrawableTransitionOptions.withCrossFade(DEFAULT_CROSS_FADE_DURATION_MILLIS))
             .diskCacheStrategy(DiskCacheStrategy.NONE)
             .into(this)
     } else {
         Glide.with(this)
             .asBitmap()
             .load(File(path))
-            .transition(BitmapTransitionOptions.withCrossFade(2000))
+            .transition(BitmapTransitionOptions.withCrossFade(DEFAULT_CROSS_FADE_DURATION_MILLIS))
+//            .transition(GenericTransitionOptions.with(R.anim.small_to_large)) Example of a customizable animation from xml
             .transform(RotateIfNeededTransformation())
             .diskCacheStrategy(DiskCacheStrategy.NONE)
             .into(this)
     }
 }
+
+private const val DEFAULT_CROSS_FADE_DURATION_MILLIS = 2000
